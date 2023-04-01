30978b037f16b363030c5d87d5a6ccc2929bb337
src48597962 <48597962@qq.com>
2023-04-01 17:01:37 +0800

    更新 'jiekou/shenmanhua.js'

--- jiekou/shenmanhua.js
+++ jiekou/shenmanhua.js
@@ -57,17 +57,17 @@ let yidata = {
 
 let erdata = {
     "链接": "https://m.taomanhua.com/",//会写入MY_HOME
-    "搜索": function (sourcename) {//做为搜索源时，聚合搜索换源列表数据,变量传入接口名称
+    "搜索": function () {//做为搜索源时，聚合搜索换源列表数据
         let d = [];
         MY_URL = MY_HOME + "api/getsortlist/?product_id=3&productname=smh&platformname=wap&orderby=click&search_key="+name+"&page=1&size=30";
         let code = JSON.parse(request(MY_URL)).data.data
         code.forEach(item => {
             if(item.comic_name.includes(name)){
                 d.push({
-                    title: '源：'+sourcename+' '+(item.comic_name==name?"":item.comic_name),
+                    title: item.comic_name,
                     desc: item.last_chapter_name,//建议取最新
                     pic_url: "https://m.taomanhua.com/static/images/favicon.ico@Referer=",//网站图标
-                    url: '',//MY_HOME + '/' + data.comic_newid
+                    url: MY_HOME + '/' + data.comic_newid,//原站二级链接
                     col_type: 'avatar'
                 });
             }
