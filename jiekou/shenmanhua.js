ac0be0d5ec5de0d2eb07c71a06e8265b9b67b264
src48597962 <48597962@qq.com>
2023-04-05 11:50:17 +0800

    更新 'jiekou/shenmanhua.js'

--- jiekou/shenmanhua.js
+++ jiekou/shenmanhua.js
@@ -1,5 +1,5 @@
 let yidata = {
-    "链接": "https://m.taomanhua.com/",//会写入MY_HOME
+    "链接": "https://m.taomanhua.com",//会写入MY_HOME
     "主页": function () {
         let d = [];
         MY_URL = MY_HOME;
@@ -23,32 +23,96 @@ let yidata = {
         });
         return d;
     },
-    "搜索": function () {//做为主页源时，视界自带搜索
+    "分类": function () {
         let d = [];
-        MY_URL = MY_HOME + "api/getsortlist/?product_id=3&productname=smh&platformname=wap&orderby=click&search_key=" + name + "&page=" + page + "&size=30";
-        let code = JSON.parse(request(MY_URL)).data.data
-        code.forEach(item => {
+        var 当前页 = 1;//getParam('page');
+        var 类别 = MY_RULE.title + "类别"
+        var 类别名 = getVar(类别, "");
+
+        var 排序 = MY_RULE.title + "排序"
+        var 排序名 = getVar(排序, "click");
+
+        var class_Name = MY_RULE.title + "分类"
+
+        if (当前页 == 1) {
+            if (!getVar(class_Name)) {
+                var codes = request(MY_HOME + '/sort/');
+                putVar(class_Name, codes)
+            }else{
+                var codes = getVar(class_Name)
+            }
+
+            var 分类项 = pdfa(codes, '.dl-sort-list&&a').map((data) => {
+                var 项数据 = {};
+                项数据.title = pdfh(data, 'Text')
+                项数据.bs = pdfh(data, 'a&&href').replace('/sort/', '').replace('.html', '')
+                项数据.sz = 项数据.bs == 类别名 ? true : false;
+                return 项数据;
+            })
+
+            var 排序项 = pdfa(codes, '#js_orderList&&li').map((data) => {
+                var 项数据 = {};
+                项数据.title = pdfh(data, 'Text')
+                项数据.bs = pdfh(data, 'li&&data-sort')
+                项数据.sz = 项数据.bs == 排序名 ? true : false;
+                return 项数据;
+            })
+
+            function List_of_options(数据源, 赋值名) {
+                d.push({
+                    col_type: 'blank_block'
+                });
+                数据源.forEach((data) => {
+                    var title = data.title;
+                    if (data.sz) {
+                        title = '““””<b><font color=#FA7298>' + title + '</font></b>';
+                    }
+
+                    var url_qz = $("#noLoading#").lazyRule((list_name, Url) => {
+                        putVar(list_name, Url)
+                        refreshPage(false);
+                        return "hiker://empty"
+                    }, 赋值名, data.bs)
+                    d.push({
+                        title: title,
+                        url: url_qz,
+                        col_type: "scroll_button"
+                    });
+                })
+            }
+
+            List_of_options(分类项, 类别)
+            List_of_options(排序项, 排序)
+
+        }
+
+        var 分类post = MY_HOME + '/api/getsortlist/?product_id=3&productname=smh&platformname=wap&orderby=@@&search_key=&comic_sort=**&size=30&page=~~'
+        var code = JSON.parse(request(分类post.replace('**', 类别名).replace('@@', 排序名).replace('~~', 当前页))).data.data;
+
+        code.forEach((data) => {
             d.push({
-                title: item.comic_name,
-                desc: '最新：' + item.last_chapter_name,
-                pic_url: item.cover_img + "@Referer=",
-                content: item.cartoon_desc,
+                title: data.comic_name,
+                desc: data.last_chapter_name,
+                pic_url: data.cover_img + "@Referer=",
+                url: 'hiker://empty#immersiveTheme##autoCache##noHistory#?url=' + 前缀 +'/'+ data.comic_newid + '/@rule=js:$.require("hiker://page/details")',
+                col_type: "movie_3_marquee",
                 extra: {
-                    name: item.comic_name,
-                    img: item.cover_img
+                    name: data.comic_name,
+                    url: 前缀 +'/'+ data.comic_newid +'/',
+                    qz: 前缀
                 }
             });
-        });
+        })
         return d;
     }
 }
 
 let erdata = {
     "作者": "嗨又是我",//接口作者
-    "链接": "https://m.taomanhua.com/",//会写入MY_HOME
+    "链接": "https://m.taomanhua.com",//会写入MY_HOME
     "搜索": function () {//做为搜索源时，聚合搜索换源列表数据
         let d = [];
-        MY_URL = MY_HOME + "api/getsortlist/?product_id=3&productname=smh&platformname=wap&orderby=click&search_key=" + name + "&page=1&size=30";
+        MY_URL = MY_HOME + "/api/getsortlist/?product_id=3&productname=smh&platformname=wap&orderby=click&search_key=" + name + "&page=1&size=30";
         let code = JSON.parse(request(MY_URL)).data.data
         code.forEach(item => {
             if (item.comic_name.includes(name)) {
