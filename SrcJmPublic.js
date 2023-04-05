af8d6534b6f735dbf2ebbc330366cc44b521dbc7
src48597962 <48597962@qq.com>
2023-04-05 16:24:38 +0800

    更新 'SrcJmPublic.js'

--- SrcJmPublic.js
+++ SrcJmPublic.js
@@ -21,3 +21,51 @@ let yidatalist = datalist.filter(it=>{
 let erdatalist = datalist.filter(it=>{
     return it.erparse;
 });
+
+function getData(type) {
+    let d = [];
+    let sourcedata = yidatalist.filter(it=>{
+        return it.name==yijisource;
+    });
+    if(sourcedata.length==0){
+        d.push({
+            title: "请先配置一个主页源",
+            url: "hiker://empty",
+            col_type: "text_center_1",
+        })
+    }else{
+        let parse;
+        eval("let source = " + sourcedata[0].parse);
+        if(source.ext && /^http/.test(source.ext)){
+            requireCache(source.ext, 48);
+            parse = yidata;
+        }else{
+            parse = source;
+        }
+        let data = [];
+        try{
+            eval("let 数据 = " + parse[type])
+            data = 数据();
+        }catch(e){
+            log(e.message);
+        }
+        if(data.length==0){
+            data.push({
+                title: "未获取到数据",
+                url: "hiker://empty",
+                col_type: "text_center_1",
+            })
+        }
+        data.forEach(item => {
+            if(item.pic_url){
+                item.extra = {name: item.extra&&item.extra.name?item.extra.name:item.title, img: item.pic_url}
+            }
+            item.url = item.url || $('hiker://empty#immersiveTheme##autoCache#').rule(() => {
+                require(config.依赖.match(/http(s)?:\/\/.*\//)[0] + 'SrcJuman.js');
+                erji();
+            })
+        })
+        d = d.concat(data);
+    }
+    setResult(d);
+}
