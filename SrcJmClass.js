a168cc9cfbf02bf9b68189764bdf5cc565bae82f
src48597962 <48597962@qq.com>
2023-04-05 15:20:23 +0800

    更新 'SrcJmClass.js'

--- SrcJmClass.js
+++ SrcJmClass.js
@@ -2,7 +2,50 @@
 require(config.依赖.match(/http(s)?:\/\/.*\//)[0] + 'SrcJmPublic.js');
 //分类
 function Update() {
-
+    let d = [];
+    let sourcedata = yidatalist.filter(it=>{
+        return it.name==yijisource;
+    });
+    if(sourcedata.length==0){
+        d.push({
+            title: "请先配置一个主页源",
+            url: "hiker://empty",
+            col_type: "text_center_1",
+        })
+    }else{
+        let parse;
+        eval("let source = " + sourcedata[0].parse);
+        if(source.ext && /^http/.test(source.ext)){
+            requireCache(source.ext, 48);
+            parse = yidata;
+        }else{
+            parse = source;
+        }
+        let data = [];
+        try{
+            eval("let 更新 = " + parse['更新'])
+            data = 更新();
+        }catch(e){
+            log(e.message);
+        }
+        if(data.length==0){
+            data.push({
+                title: "未获取到数据",
+                url: "hiker://empty",
+                col_type: "text_center_1",
+            })
+        }
+        data.forEach(item => {
+            if(item.pic_url){
+                item.extra = {name: item.title, img: item.pic_url}
+            }
+            item.url = item.url || $('hiker://empty#immersiveTheme##autoCache#').rule(() => {
+                require(config.依赖.match(/http(s)?:\/\/.*\//)[0] + 'SrcJuman.js');
+                erji();
+            })
+        })
+        d = d.concat(data);
+    }
     setResult(d);
 }
 //分类
