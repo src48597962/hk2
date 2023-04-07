570a0151751ff73405c1568810cae8336f7f04e0
src48597962 <48597962@qq.com>
2023-04-07 11:50:51 +0800

    更新 'SrcJu.js'

--- SrcJu.js
+++ SrcJu.js
@@ -119,6 +119,7 @@ function erji() {
     let d = [];
     let parse;
     let stype = MY_PARAMS.stype;
+    log(getMark(name,stype))
     let erjiextra = storage0.getMyVar('erjiextra') || getMark(name,stype) || MY_PARAMS;
     let sname = erjiextra.sname || "";
     let surl = erjiextra.surl || "";
