9ff5ed247e4b54c6370b8fcbb2068ff548033d2a
src48597962 <48597962@qq.com>
2023-04-08 10:47:00 +0800

    更新 'SrcJu.js'

--- SrcJu.js
+++ SrcJu.js
@@ -138,10 +138,10 @@ function yiji() {
                         let mask = it.lastClick?it.lastClick.split('@@')[0]:"";
                         let col = Juconfig["bookCase_col_type"] || 'movie_1_vertical_pic';
                         d.push({
-                            title: col=='movie_1_vertical_pic'?name + "\n\n‘‘’’<small>💠 "+stype+"|"+sname+"</small>":name,
+                            title: col=='movie_1_vertical_pic'?name + "\n\n‘‘’’<small>💠 "+stype+" | "+sname+"</small>":name,
                             pic_url: it.picUrl,
                             desc: col=='movie_1_vertical_pic'?"🕓 "+mask+"\n\n🔘 "+last:last,
-                            url: $('hiker://empty#immersiveTheme##autoCache#').rule(() => {
+                            url: $(it.cUrl).rule(() => {
                                 require(config.依赖);
                                 erji();
                             }),
