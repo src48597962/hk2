f352a4a4730a6709ee5cb4979a359b4daf7d4a56
src48597962 <48597962@qq.com>
2023-04-09 10:42:33 +0800

    更新 'jiekou/shenmanhua.js'

--- jiekou/shenmanhua.js
+++ jiekou/shenmanhua.js
@@ -2,7 +2,7 @@ let yidata = {
     "作者": "嗨又是我",//接口作者
     "页码": {"分类":1, "排行":0, "更新":0},//页码元素可不传，如果传1则会传fypage，用getParam('page')获取
     "主页": function () {
-        log(公共);
+        //log(公共);
         let d = [];
         MY_URL = "https://m.taomanhua.com";
         let html = request(MY_URL);
