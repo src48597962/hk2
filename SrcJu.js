c3fc0e31b6cf397c2cf8a35fe61d8bb9588a9972
src48597962 <48597962@qq.com>
2023-04-12 22:26:44 +0800

    更新 'SrcJu.js'

--- SrcJu.js
+++ SrcJu.js
@@ -33,7 +33,9 @@ function yiji() {
     } catch (e) {
         log("一级源接口加载异常>" + e.message);
     }
-
+    require(config.cd);
+    let cd = readData(runMode+"_"+sourcename,"一级");
+    log(cd)
     页码 = 页码 || {};
     let d = [];
     if(MY_PAGE==1){
