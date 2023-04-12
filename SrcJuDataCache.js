bf685400c5f48e514ee19b607f5c9786dc6ad999
src48597962 <48597962@qq.com>
2023-04-12 16:34:08 +0800

    更新 'SrcJuDataCache.js'

--- SrcJuDataCache.js
+++ SrcJuDataCache.js
@@ -2,5 +2,5 @@ function cacheData(data){
     let jkdata = data;
     let cfilename = jkdata.name+'-'+jkdata.type;
     let cachefile = `hiker://files/cache/_fileSelect_${cfilename}.json`;
-    writeFile(cachefile,jkdata);
+    writeFile(cachefile,JSON.stringify(jkdata));
 }
