669e8551ad0757fd9cb1ccf5c4451f523ab9d402
src48597962 <48597962@qq.com>
2023-04-12 16:46:38 +0800

    更新 'SrcJuDataCache.js'

--- SrcJuDataCache.js
+++ SrcJuDataCache.js
@@ -4,3 +4,21 @@ function cacheData(data){
     let cachefile = `hiker://files/cache/_fileSelect_${cfilename}.json`;
     writeFile(cachefile,JSON.stringify(jkdata));
 }
+
+function readData(jkid){
+    let cfilename = jkid;
+    let cachefile = `hiker://files/cache/_fileSelect_${cfilename}.json`;
+    let cachedata = fetch(cachefile);
+    if(cachedata != ""){
+        try{
+            eval("var jkdata=" + cachedata+ ";");
+        }catch(e){
+            var jkdata = {};
+        }
+    }else{
+        var jkdata = {};
+    }
+    let 公共 = jkdata.public;
+    let 一级 = jkdata.parse;
+    let 二级 = jkdata.erparse;
+}
