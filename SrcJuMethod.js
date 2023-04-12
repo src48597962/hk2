ad1ccddabba3182827ac3d89870b7e1bba5a87b6
src48597962 <48597962@qq.com>
2023-04-12 20:15:33 +0800

    更新 'SrcJuMethod.js'

--- SrcJuMethod.js
+++ SrcJuMethod.js
@@ -1,12 +1,13 @@
-function toerji(item,sname,stype) {
+function toerji(item) {
+    let info = storage0.getMyVar('主页源信息');
     let extra = item.extra || {};
     extra.name = extra.name || extra.pageTitle ||item.title;
     extra.img = extra.img || item.pic_url || item.img;
-    extra.stype = stype;
+    extra.stype = info.type;
     extra.pageTitle = extra.pageTitle || extra.name;
     if(item.url && !/js:|select:|\(|\)|=>|@/.test(item.url)){
         extra.surl = item.url.replace(/hiker:\/\/empty|#immersiveTheme#|#autoCache#|#noRecordHistory#|#noHistory#|#noLoading#|#/g,"");
-        extra.sname = sname;
+        extra.sname = info.name;
     }
     if((item.col_type!="scroll_button") || item.extra){
         item.extra = extra;
