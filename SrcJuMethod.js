3f993346709b244668b12096412136254ac9ba9e
src48597962 <48597962@qq.com>
2023-04-13 15:32:26 +0800

    更新 'SrcJuMethod.js'

--- SrcJuMethod.js
+++ SrcJuMethod.js
@@ -24,3 +24,32 @@ function jianfan(str,x) {
     require(config.依赖.match(/http(s)?:\/\/.*\//)[0] + 'SrcSimple.js');
     return PYStr(str,x);
 }
+//写接口数据临时缓存
+function cacheData(jkdata){
+    let fileid = jkdata.type + '_'+ jkdata.name;
+    let cachefile = `hiker://files/cache/_fileSelect_${fileid}.json`;
+    if (!fileExist(cachefile)) {
+        writeFile(cachefile,JSON.stringify(jkdata));
+    }
+}
+//读接口数据临时缓存
+function readData(fileid,datatype){
+    let cachefile = `hiker://files/cache/_fileSelect_${fileid}.json`;
+    let cachedata = fetch(cachefile);
+    if(cachedata != ""){
+        try{
+            eval("var jkdata=" + cachedata+ ";");
+        }catch(e){
+            var jkdata = {};
+        }
+    }else{
+        var jkdata = {};
+    }
+    if(datatype=="1"){
+        return jkdata.parse;
+    }else if(datatype=="2"){
+        return jkdata.erparse;
+    }else if(datatype=="0"){
+        return jkdata.public;
+    }
+}
