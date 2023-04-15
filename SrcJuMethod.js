16e3ab62b6f39a0d1fb0ff171f183c8c03eee0f9
src48597962 <48597962@qq.com>
2023-04-16 00:04:14 +0800

    更新 'SrcJuMethod.js'

--- SrcJuMethod.js
+++ SrcJuMethod.js
@@ -71,3 +71,20 @@ function readData(fileid,datatype){
         return "";
     }
 }
+let 一级 = function(fileid) {
+    let info = storage0.getMyVar('一级源接口信息');
+    let 标识 = info.type + "_" + info.name;
+    fileid = fileid || 标识;
+    return readData(fileid, 1)
+}
+let 二级 = function(fileid) {
+    return readData(fileid, 2)
+}
+let 公共 = function(fileid) {
+    return readData(fileid, 3)
+}
+let 属性 = function(fileid, parse, attribut) {
+    let 接口;
+    eval("接口 = " + parse);
+    return 接口(fileid)[attribut];
+};
