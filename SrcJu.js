5e6b25df832566c51e62958251edb4def3a13946
src48597962 <48597962@qq.com>
2023-04-22 21:18:44 +0800

    更新 'SrcJu.js'

--- SrcJu.js
+++ SrcJu.js
@@ -255,7 +255,8 @@ function erji() {
             //方便换源时二级代码中使用MY_PARAMS
             MY_PARAMS = erjiextra;
             log(公共)
-            details = detailsmark || parse['二级'](surl);
+            eval("let 二获获取 = " + parse['二级'])
+            details = detailsmark || 二获获取(surl);
             let pic = details.img || MY_PARAMS.img || "https://p1.ssl.qhimgs1.com/sdr/400__/t018d6e64991221597b.jpg";
             pic = pic.indexOf("@Referer=") == -1 ? pic + "@Referer=" : pic;
             d.push({
