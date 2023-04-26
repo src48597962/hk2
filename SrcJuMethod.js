674f81f5f1be61f5d006b10448c4bfa289e6c3e1
src48597962 <48597962@qq.com>
2023-04-26 16:28:04 +0800

    更新 'SrcJuMethod.js'

--- SrcJuMethod.js
+++ SrcJuMethod.js
@@ -33,68 +33,66 @@ function cacheData(jkdata){
     }
 }
 //子页面读接口临时缓存
-let jiekoupage = {
-    readData: function (fileid,datatype) {
-        let cachefile = `hiker://files/_cache/${fileid}.json`;
-        let jkdata = {};
-        try{
-            eval("jkdata=" + fetch(cachefile));
-        }catch(e){
-            log("jkdata加载失败>"+fileid+">"+e.message);
-        }
+function readData(fileid,datatype){
+    let cachefile = `hiker://files/_cache/${fileid}.json`;
+    let jkdata = {};
+    try{
+        eval("jkdata=" + fetch(cachefile));
+    }catch(e){
+        log("jkdata加载失败>"+fileid+">"+e.message);
+    }
 
-        try{
-            let parse;
-            let source;
-            if(datatype==1){
-                eval("source = " + jkdata.parse);
-                if (source && source.ext && /^http/.test(source.ext)) {
-                    requireCache(source.ext, 48);
-                    parse = yidata;
-                } else {
-                    parse = source;
-                }
-            }else if(datatype==2){
-                eval("source = " + jkdata.erparse);
-                if (source && source.ext && /^http/.test(source.ext)) {
-                    requireCache(source.ext, 48);
-                    parse = erdata;
-                } else {
-                    parse = source;
-                }
-            }else if(datatype==3){
-                eval("source = " + jkdata.public);
-                if (source && source.ext && /^http/.test(source.ext)) {
-                    requireCache(source.ext, 48);
-                    parse = ggdata;
-                } else {
-                    parse = source;
-                }
+    try{
+        let parse;
+        let source;
+        if(datatype==1){
+            eval("source = " + jkdata.parse);
+            if (source && source.ext && /^http/.test(source.ext)) {
+                requireCache(source.ext, 48);
+                parse = yidata;
+            } else {
+                parse = source;
             }
-            return parse;
-        }catch(e){
-            log("读取接口本地缓存文件失败>"+e.message);
-            return "";
+        }else if(datatype==2){
+            eval("source = " + jkdata.erparse);
+            if (source && source.ext && /^http/.test(source.ext)) {
+                requireCache(source.ext, 48);
+                parse = erdata;
+            } else {
+                parse = source;
+            }
+        }else if(datatype==3){
+            eval("source = " + jkdata.public);
+            if (source && source.ext && /^http/.test(source.ext)) {
+                requireCache(source.ext, 48);
+                parse = ggdata;
+            } else {
+                parse = source;
+            }
         }
-    },
-    一级: function(fileid) {
-        let info = storage0.getMyVar('一级源接口信息') || {};
-        let 标识 = info.type + "_" + info.name;
-        fileid = fileid || 标识;
-        return readData(fileid, 1)
-    },
-    二级: function(fileid) {
-        let info = storage0.getMyVar('二级源接口信息') || {};
-        let 标识 = info.type + "_" + info.name;
-        fileid = fileid || 标识;
-        return readData(fileid, 2)
-    },
-    公共: function(fileid) {
-        return readData(fileid, 3)
-    },
-    属性: function(fileid, parse, attribut) {
-        let 接口;
-        eval("接口 = " + parse);
-        return 接口(fileid)[attribut];
-    }            
+        return parse;
+    }catch(e){
+        log("读取接口本地缓存文件失败>"+e.message);
+        return "";
+    }
 }
+let 一级 = function(fileid) {
+    let info = storage0.getMyVar('一级源接口信息') || {};
+    let 标识 = info.type + "_" + info.name;
+    fileid = fileid || 标识;
+    return readData(fileid, 1)
+}
+let 二级 = function(fileid) {
+    let info = storage0.getMyVar('二级源接口信息') || {};
+    let 标识 = info.type + "_" + info.name;
+    fileid = fileid || 标识;
+    return readData(fileid, 2)
+}
+let 公共 = function(fileid) {
+    return readData(fileid, 3)
+}
+let 属性 = function(fileid, parse, attribut) {
+    let 接口;
+    eval("接口 = " + parse);
+    return 接口(fileid)[attribut];
+};
