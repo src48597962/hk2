d066d49ea2c6dfc1f50c2da781c3eb492b0b6027
src48597962 <48597962@qq.com>
2023-04-28 15:45:31 +0800

    更新 'SrcJu.js'

--- SrcJu.js
+++ SrcJu.js
@@ -296,10 +296,10 @@ function erji() {
                 }
             })
             detailload = 1;
-            
             lineid = lineid || getMyVar("SrcJu_"+surl, '0');
             let 线路s = details.line?details.line:["线路"];
             let 列表s = details.line?details.list:[details.list];
+            /*
             try{
                 pageid = pageid || getMyVar("SrcJu_"+surl+"#page", '0');
                 if(details.page && pageid>0){
@@ -315,6 +315,7 @@ function erji() {
             }catch{e}{
                 log('√'+sname+'分页选集处理失败>'+e.message);
             }
+            */
             try{
                 if(lineid > 线路s.length){
                     lineid = 0;
