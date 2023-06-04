7276c5f126346136d1d0767f285334cfc08a75a1
src48597962 <48597962@qq.com>
2023-06-04 13:26:43 +0800

    更新 'SrcJu.js'

--- SrcJu.js
+++ SrcJu.js
@@ -715,7 +715,6 @@ function erji() {
         //切换源时更新收藏数据，以及分享时附带接口
         if (typeof (setPageParams) != "undefined") {
             if ((surl && oldMY_PARAMS.surl!=surl) || !oldMY_PARAMS.sourcedata) {
-                log('1');
                 delete sourcedata2['parse']
                 erjiextra.sourcedata = sourcedata2;
                 setPageParams(erjiextra);
