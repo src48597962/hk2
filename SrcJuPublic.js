ba870a8a22a4c898325b96fd089ae340603052f0
src48597962 <48597962@qq.com>
2023-07-02 21:26:40 +0800

    更新 'SrcJuPublic.js'

--- SrcJuPublic.js
+++ SrcJuPublic.js
@@ -108,7 +108,7 @@ function getYiData(type,od) {
                         return "hiker://page/download.view#noRecordHistory##noRefresh##noHistory#?rule=本地资源管理"
                     })
                 }];
-                extra.chapterList = [{title:"下载图集", url:item.url.split("@")[0]}],
+                extra.chapterList = [{title:"正文", url:item.url.split("@")[0]}],
                 extra.defaultView = "1";
                 extra.info = {
                     "bookName": extra.name,
