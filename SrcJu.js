a43dc5ede95900c08e2fceab6c14995ee3a88a29
src48597962 <48597962@qq.com>
2023-07-03 17:27:43 +0800

    更新 'SrcJu.js'

--- SrcJu.js
+++ SrcJu.js
@@ -1009,7 +1009,7 @@ function search(keyword, mode, sdata, group, type) {
                         }, extra);
                         item.title = objmode=="erji"?objdata.name:item.title;
                         //item.content = item.content || item.desc;
-                        item.desc = objmode=="sousuo"  ? MY_RULE.title + ' · ' + objdata.name + ' · ' +item.desc :(objmode=="sousuotest"||objmode=="sousuo2")?(item.content || item.desc): (extra.sdesc || item.desc);
+                        item.desc = objmode=="sousuo"||objmode=="sousuo2"  ? MY_RULE.title+' · '+objdata.name+' · '+item.desc :objmode=="sousuotest"?(item.content || item.desc): (extra.sdesc || item.desc);
                         item.col_type = objmode=="sousuo"  ? "video" : (objmode=="sousuotest"||objmode=="sousuo2") ? "movie_1_vertical_pic" : "avatar";
                         resultdata.push(item);
                     }
