2b492d069deb5ecd2a378208e5f81d8acccbd440
src48597962 <48597962@qq.com>
2023-07-05 16:37:18 +0800

    更新 'SrcJu.js'

--- SrcJu.js
+++ SrcJu.js
@@ -879,7 +879,7 @@ function sousuo() {
                     data.push({
                         "title": it.name,
                         "search_url": "hiker://empty##fypage",
-                        "searchFind": `js: require(config.依赖); let d = search('`+name+`','jusousuo',JSON.parse('`+JSON.stringify(it)+`')); setResult(d);`
+                        "searchFind": `js: require(config.依赖); let d = search('`+name+`','jusousuo','`+JSON.stringify(it)+`'); setResult(d);`
                     });
                 })
                 return JSON.stringify(data)
