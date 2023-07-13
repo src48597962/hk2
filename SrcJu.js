77124983ff04fe2635987ba2629e96abca2b8a9e
src48597962 <48597962@qq.com>
2023-07-13 11:41:29 +0800

    更新 'SrcJu.js'

--- SrcJu.js
+++ SrcJu.js
@@ -66,18 +66,6 @@ function yiji() {
             Version();
             downloadicon();
         }
-        runModes.forEach((it) =>{
-            d.push({
-                title: getMyVar("sousuoPageType",runMode)==it?`““””<b><span style="color: #3399cc">`+it+`</span></b>`:it,
-                url: $('#noLoading#').lazyRule((it) => {
-                    putMyVar("sousuoPageType",it);
-                    initConfig({依赖: getMyVar('SrcJuCfg')});
-                    refreshPage(false);
-                    return "hiker://empty";
-                },it),
-                col_type: 'text_5'
-            });
-        })
         let adminbtn = Object.assign([],runModes);
         adminbtn.unshift("快速切换");
         adminbtn.unshift("接口管理");
@@ -138,7 +126,7 @@ function yiji() {
                 }
             }),
             pic_url: "https://hikerfans.com/tubiao/more/129.png",
-            col_type: 'scroll_button',//icon_5
+            col_type: 'icon_5',
             extra: {
                 newWindow: true,
                 windowId: MY_RULE.title + "管理",
@@ -160,14 +148,14 @@ function yiji() {
                 title: "排行",
                 url: rulePage('排行',页码["排行"]),
                 pic_url: "https://hikerfans.com/tubiao/more/229.png",
-                col_type: 'scroll_button'
+                col_type: 'icon_5'
             })
         }else{
             d.push({
                 title: "收藏",
                 url: "hiker://collection?rule="+MY_RULE.title,
                 pic_url: "https://hikerfans.com/tubiao/more/109.png",
-                col_type: 'scroll_button'
+                col_type: 'icon_5'
             })
         }
         let sousuopage = $("hiker://empty#noRecordHistory##noHistory##fullTheme###fypage").rule(() => {
@@ -213,7 +201,7 @@ function yiji() {
                 title: "分类",
                 url: rulePage('分类',页码["分类"]),
                 pic_url: "https://hikerfans.com/tubiao/more/287.png",
-                col_type: 'scroll_button',
+                col_type: 'icon_5',
                 extra: sousuoextra
             })
         }else{
@@ -221,7 +209,7 @@ function yiji() {
                 title: "搜索",
                 url: sousuopage,
                 pic_url: "https://hikerfans.com/tubiao/more/101.png",
-                col_type: 'scroll_button',
+                col_type: 'icon_5',
                 extra: sousuoextra
             })
         }
@@ -230,14 +218,14 @@ function yiji() {
                 title: "更新",
                 url: rulePage('更新',页码["更新"]),
                 pic_url: "https://hikerfans.com/tubiao/more/288.png",
-                col_type: 'scroll_button'
+                col_type: 'icon_5'
             })
         }else{
             d.push({
                 title: "历史",
                 url: "hiker://history?rule="+MY_RULE.title,
                 pic_url: "https://hikerfans.com/tubiao/more/213.png",
-                col_type: 'scroll_button'
+                col_type: 'icon_5'
             })
         }
         
@@ -248,7 +236,7 @@ function yiji() {
                 bookCase();
             }),
             pic_url: "https://hikerfans.com/tubiao/more/286.png",
-            col_type: 'scroll_button',
+            col_type: 'icon_5',
             extra: {
                 longClick: [{
                     title: "切换按钮",
@@ -266,6 +254,18 @@ function yiji() {
         })
         d.push({
             col_type: 'blank_block'
+        })
+        runModes.forEach((it) =>{
+            d.push({
+                title: getMyVar("sousuoPageType",runMode)==it?`““””<b><span style="color: #3399cc">`+it+`</span></b>`:it,
+                url: $('#noLoading#').lazyRule((it) => {
+                    putMyVar("sousuoPageType",it);
+                    initConfig({依赖: getMyVar('SrcJuCfg')});
+                    refreshPage(false);
+                    return "hiker://empty";
+                },it),
+                col_type: 'scroll_button'
+            });
         })
         putMyVar(runMode+"_"+sourcename, "1");
     }
