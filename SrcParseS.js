64889e8e99b2311cf06933261bdd703023f3c7d7
src48597962 <48597962@qq.com>
2023-07-16 00:12:07 +0800

    更新 'SrcParseS.js'

--- SrcParseS.js
+++ SrcParseS.js
@@ -1,14 +1,6 @@
 function 聚阅(vipUrl) {
     if (vipUrl.indexOf("https://www.aliyundrive.com") > -1) {
-        let cfgfile = "hiker://files/rules/Src/config.json";
-        let juyingLink;
-        eval("let JYconfig=" + (fetch(cfgfile)||"{}") + ";");
-        if(!JYconfig["juying"]){
-            JYconfig["juying"] = config.依赖.match(/http(s)?:\/\/.*\//)[0].replace('/Ju/','/master/');
-            writeFile(cfgfile, JSON.stringify(Juconfig));
-        }
-        juyingLink = JYconfig["juying"];
-        require(juyingLink + 'SrcJyAliDisk.js');
+        require(config.依赖.match(/http(s)?:\/\/.*\//)[0].replace('/Ju/','/master/') + 'SrcJyAliDisk.js');
         aliShareUrl(vipUrl)
     }
 }
