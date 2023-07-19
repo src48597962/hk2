db19c88fdb5c5a8a715633d9d8c5db4fcae060fe
src48597962 <48597962@qq.com>
2023-07-19 16:17:58 +0800

    更新 'SrcJu.js'

--- SrcJu.js
+++ SrcJu.js
@@ -791,7 +791,10 @@ function erji() {
 
             let list_col_type = getItem('SrcJuList_col_type', 'text_2');//列表样式
             for(let i=0; i<列表.length; i++) {
-                let extra = details["extra"] || {};
+                let extra = 列表[i].extra || {};
+                try{
+                    extra = Object.assign(extra, details["extra"] || {});
+                }catch(e){}
                 extra.id = name + "_选集_" + i;
                 extra.url = 列表[i].url;
                 extra.cls = "loadlist playlist";
