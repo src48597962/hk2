eda6a37f51fd9fd65cb13dcc725e3c2c259feb95
src48597962 <48597962@qq.com>
2023-08-06 22:32:35 +0800

    更新 'SrcJuSet.js'

--- SrcJuSet.js
+++ SrcJuSet.js
@@ -96,7 +96,7 @@ function SRCSet() {
                     JYimport(input)
                 })
             }else if(input=="文件导入"){
-                return `fileSelect://require(config.依赖.match(/http(s)?:\/\/.*\//)[0] + 'SrcJuSet.js');JYimport(input);`
+                return `fileSelect://log(input)`
             }
         }),
         img: "https://hikerfans.com/tubiao/more/43.png",
