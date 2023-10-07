7f5f8c5f2fa36fdadbfcd93b176dadf30ee4c7a6
src48597962 <48597962@qq.com>
2023-10-07 21:04:48 +0800

    更新 'SrcJu.js'

--- SrcJu.js
+++ SrcJu.js
@@ -275,13 +275,17 @@ function yiji() {
             col_type: 'blank_block'
         })
         putMyVar(runMode+"_"+sourcename, "1");
-    }
-    if (typeof(setPreResult)!="undefined") {
-        setPreResult(d);
+        if (typeof(setPreResult)!="undefined") {
+            setPreResult(d);
+            showLoading("正在加载中");
+            d = [];
+        }
     }
     try{
         getYiData('主页', d);
+        hideLoading();
     }catch(e){
+        hideLoading();
         toast("当前主页源有报错，可更换主页源或联系接口作者");
         xlog("√"+提示);
         xlog("√当前主页源报错信息>"+e.message);
