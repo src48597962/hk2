dac570a4ca8d973d3ea22bfb2181fe52c71bf396
src48597962 <48597962@qq.com>
2023-10-08 09:02:54 +0800

    更新 'SrcJu.js'

--- SrcJu.js
+++ SrcJu.js
@@ -1072,7 +1072,7 @@ function search(keyword, mode, sdata, group, type) {
                 ssdata.forEach(item => {
                     let extra = item.extra || {};
                     extra.name = extra.name || extra.pageTitle || (item.title?item.title.replace(/‘|’|“|”|<[^>]+>|全集|国语|粤语/g,"").trim():"");
-                    if((objmode=="erji" && ((getItem('searchMatch','1')=="1"&&extra.name.toLowerCase()==name.toLowerCase())||extra.name.toLowerCase().includes(name.toLowerCase()))) || objmode!="erji"){
+                    if((objmode=="erji" && ((getItem('searchMatch','1')=="1"&&extra.name.toLowerCase()==name.toLowerCase())||(getItem('searchMatch')=="2"&&extra.name.toLowerCase().includes(name.toLowerCase())))) || objmode!="erji"){
                         let keepurl = /js:|select:|\(|\)|=>|hiker:\/\/page|toast:/;//定义保留传值的项目url
                         if((!keepurl.test(item.url) && extra.name.toLowerCase().includes(name.toLowerCase())) || keepurl.test(item.url)){
                             extra.img = extra.img || item.img || item.pic_url;
