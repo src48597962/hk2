515309b7d46d4613599fa9c57111c9edb4aac6b0
src48597962 <48597962@qq.com>
2023-10-22 17:10:20 +0800

    更新 'SrcJuConfig.js'

--- SrcJuConfig.js
+++ SrcJuConfig.js
@@ -4,9 +4,18 @@ if(Jucfg != ""){
     eval("var Juconfig=" + Jucfg+ ";");
 }else{
     var Juconfig= {};
+    Juconfig["依赖"] = config.依赖 || "https://gitcode.net/src48597962/hk/-/raw/Ju/SrcJuPublic.js";
+    writeFile(cfgfile, JSON.stringify(Juconfig));
 }
-let sourcename = Juconfig[runMode+'sourcename'] || "";
-let erji = $('hiker://empty#immersiveTheme##autoCache#').rule(() => {
-    require(config.依赖);
-    erji();
-})
+let sourcefile = "hiker://files/rules/Src/Ju/jiekou.json";
+let sourcedata = fetch(sourcefile);
+if(sourcedata != ""){
+    try{
+        eval("var datalist=" + sourcedata+ ";");
+    }catch(e){
+        var datalist = [];
+    }
+}else{
+    var datalist = [];
+}
+datalist.reverse();
