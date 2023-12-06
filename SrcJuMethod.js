b05fe259c23d92b9d7dba3929dd534ab74fafa75
src48597962 <48597962@qq.com>
2023-12-06 08:35:15 +0800

    更新 'SrcJuMethod.js'

--- SrcJuMethod.js
+++ SrcJuMethod.js
@@ -16,7 +16,8 @@ function toerji(item,info) {
         })
     }
     if((item.col_type!="scroll_button") || item.extra){
-        item.extra = extra;
+        //item.extra = extra;
+        item.extra = Object.assign(item.extra, extra);
     }
     return item;
 }
