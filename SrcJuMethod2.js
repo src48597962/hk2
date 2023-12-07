784e6ef6a517cfde06635e6a5a9bdab5a1b6a027
src48597962 <48597962@qq.com>
2023-12-07 15:07:50 +0800

    更新 'SrcJuMethod2.js'

--- SrcJuMethod2.js
+++ SrcJuMethod2.js
@@ -118,7 +118,7 @@ let 图片解密 = function(key,iv,kiType,mode) {
         } else if (kiType === "Hex") {
             bytes = hexStringToBytes(str);
         } else {
-            bytes = FileUtil.String(str).getBytes();
+            bytes = String(str).FileUtil.getBytes();
             /*
             let javaImport = new JavaImporter();
             javaImport.importPackage(
