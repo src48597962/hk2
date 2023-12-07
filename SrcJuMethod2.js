15354d081f79ea8427024d42c0c6807f145a433a
src48597962 <48597962@qq.com>
2023-12-07 17:10:22 +0800

    更新 'SrcJuMethod2.js'

--- SrcJuMethod2.js
+++ SrcJuMethod2.js
@@ -155,7 +155,8 @@ let exportskeys = Object.keys(exports);
 let arr = 公共().exports || [];
 arr.forEach(it => {
     if(!exportskeys.includes(it.key)){
-        log(eval('('+it.type[it.key]+')'));
+
+        log(eval(it.type[it.key]));
         exports[it.key] = eval('('+it.type[it.key]+')');
     }
 })
