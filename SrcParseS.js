7143417288fa61ddc9788b3408a5b035220fde69
src48597962 <48597962@qq.com>
2024-01-23 17:46:08 +0800

    更新 'SrcParseS.js'

--- SrcParseS.js
+++ SrcParseS.js
@@ -1,5 +1,5 @@
 function JYLazy(vipUrl) {
-    if (vipUrl.indexOf("https://www.aliyundrive.com") > -1) {
+    if (/www\.aliyundrive\.com|www\.alipan\.com/.test(vipUrl)) {
         require(config.依赖.match(/http(s)?:\/\/.*\//)[0].replace('/Ju/','/master/') + 'SrcJyAliDisk.js');
         aliShareUrl(vipUrl)
     }
