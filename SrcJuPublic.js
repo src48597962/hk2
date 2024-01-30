122565e02dadb9714e0bf22f85c1b95110136b1d
src48597962 <48597962@qq.com>
2024-01-30 17:18:31 +0800

    更新 'SrcJuPublic.js'

--- SrcJuPublic.js
+++ SrcJuPublic.js
@@ -11,6 +11,7 @@ if(Jucfg != ""){
 let runModes = ["漫画","小说","听书","图集","影视","其它"];
 let jkGroupType = getItem('jkGroupType','1'); //接口列表分组列表类型
 let runMode = Juconfig["runMode"] || "漫画";
+let runType = Juconfig["runType"] || "漫画";
 let sourcename = Juconfig[runMode+'sourcename'] || "";//主页源名称
 
 let sourcefile = "hiker://files/rules/Src/Ju/jiekou.json";
