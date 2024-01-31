16e24fa8b6d1551502809a60a1f7cf6c015a20cb
src48597962 <48597962@qq.com>
2024-01-31 22:41:34 +0800

    更新 'SrcJuPublic.js'

--- SrcJuPublic.js
+++ SrcJuPublic.js
@@ -345,8 +345,8 @@ function getYiData(datatype,od) {
     }else{
         if(datatype=="主页"){
             d.push({
-                title: runMode+" 主页源未配置或无法访问\n可重新选择配置主页源",//\n设置-选择漫画/小说/听书/
-                desc: "点此或上面类型按钮都可选择",//设置长按菜单可以开启界面切换开关
+                title: runMode+" 主页源不存在\n需先选择配置主页源",//\n设置-选择漫画/小说/听书/
+                desc: "点此或上面类型按钮皆可选择",//设置长按菜单可以开启界面切换开关
                 url: $('#noLoading#').lazyRule((input) => {
                     require(config.依赖.match(/http(s)?:\/\/.*\//)[0] + 'SrcJuPublic.js');
                     return selectSource(input);
