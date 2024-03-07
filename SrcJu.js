3fbc8ce851201e7acbe9f66ace29262a771b2a03
src48597962 <48597962@qq.com>
2024-03-07 15:38:02 +0800

    更新 'SrcJu.js'

--- SrcJu.js
+++ SrcJu.js
@@ -1140,6 +1140,26 @@ function sousuo() {
         let info = storage0.getMyVar('一级源接口信息') || {};
         search(name,'sousuo',false,info.group);
     }else{
+        let info = storage0.getMyVar('一级源接口信息') || {};
+        require(config.依赖);
+        let ssdatalist = erdatalist.filter(it=>{
+            if(info.group=="全全" || !group){
+                return it.type==info.type;
+            }else{
+                return it.type==info.type && (it.group==info.group||it.group=="全全");
+            }
+        });
+        log(ssdatalist.length);
+        let data = [];
+        ssdatalist.forEach(it=>{
+            data.push({
+                "title": it.name,
+                "search_url": "hiker://empty##fypage",
+                "searchFind": `js: require(config.依赖); let d = search('`+name+`  `+it.name+`','jusousuo'); setResult(d);`
+            });
+        })
+        log(data);
+
         setResult([{
             title: "视界聚搜",
             url: "hiker://search?s=" + name,
@@ -1154,7 +1174,6 @@ function sousuo() {
                             return it.type==info.type && (it.group==info.group||it.group=="全全");
                         }
                     });
-                    log(ssdatalist.length);
 
                     let data = [];
                     ssdatalist.forEach(it=>{
@@ -1164,7 +1183,6 @@ function sousuo() {
                             "searchFind": `js: require(config.依赖); let d = search('`+name+`  `+it.name+`','jusousuo'); setResult(d);`
                         });
                     })
-                    log(JSON.stringify(data));
                     return JSON.stringify(data)
                 },name)
             }
