53b63d9cbfab551316fc40000a3ca2aacdd06066
src48597962 <48597962@qq.com>
2024-05-07 09:16:26 +0800

    更新 'SrcJuPublic.js'

--- SrcJuPublic.js
+++ SrcJuPublic.js
@@ -351,7 +351,8 @@ function getYiData(datatype,od) {
             let getData = [];
             try{
                 eval("let 数据 = " + 执行str);
-                getData = 数据() || [];
+                //getData = 数据() || [];
+                数据();
             }catch(e){
                 getData = [];
                 xlog('√执行获取数据报错，信息>'+e.message + " 错误行#" + e.lineNumber);
