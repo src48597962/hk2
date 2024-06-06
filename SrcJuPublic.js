af1806fbbea43f5e2d0ab1a91d6b60ff066d2784
src48597962 <48597962@qq.com>
2024-06-06 17:55:13 +0800

    更新 'SrcJuPublic.js'

--- SrcJuPublic.js
+++ SrcJuPublic.js
@@ -189,6 +189,7 @@ function selectSource2(selectType) {
                         if (i === 0) {
                             spen = spen == 3 ? 2 : 3;
                             manage.changeColumns(spen);
+                            let index = items.indexOf(items.filter(d => d.title == sourcename)[0]);
                             manage.scrollToPosition(index, true);
                         } else if (i === 1) {
                             setItem("sourceListSort", getItem('sourceListSort') == 'name' ? "" : "name");
