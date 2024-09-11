b95af30c4b12b1fe0d840d51001a353d9283bf6b
src48597962 <48597962@qq.com>
2024-09-11 12:02:52 +0800

    更新 'SrcParseS.js'

--- SrcParseS.js
+++ SrcParseS.js
@@ -4,3 +4,15 @@ function JYLazy(vipUrl) {
         aliShareUrl(vipUrl)
     }
 }
+var SrcParseS = {
+    聚阅: function (vipUrl) {
+        if(/www\.aliyundrive\.com|www\.alipan\.com/.test(vipUrl)) {
+            return $("hiker://empty#noRecordHistory##noHistory#").rule((input) => {
+                require(config.依赖.match(/http(s)?:\/\/.*\//)[0].replace('/Ju/','/master/') + 'SrcJyAliDisk.js');
+                aliShareUrl(input);
+            },vipUrl);
+        }else if(/pan\.quark\.cn|drive\.uc\.cn/.test(vipUrl)) {
+            return "hiker://page/quarkList?rule=Quark.简&realurl=" + encodeURIComponent(vipUrl) + "&sharePwd=";
+        }
+    }
+}
