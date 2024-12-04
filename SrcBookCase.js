1d7b0d1f43ac9d1e74dbdb6787477b1ee4ee1cad
src48597962 <48597962@qq.com>
2024-12-04 08:54:52 +0800

    更新 'SrcBookCase.js'

--- SrcBookCase.js
+++ SrcBookCase.js
@@ -26,7 +26,7 @@ function bookCase() {
     d.push({
         title: ' 本地下载',
         url: getMyVar("SrcJu_bookCaseType","全部")=="全部"?"hiker://page/Main.view?rule=本地资源管理":"hiker://page/Bookrack.view?rule=本地资源管理&ruleName="+MY_RULE.title+"&type="+(getMyVar("SrcJu_bookCaseType")=="漫画"?"comic":"novel"),
-        img: "https://hikerfans.com/tubiao/messy/70.svg",
+        img: "http://123.56.105.145/tubiao/messy/70.svg",
         col_type: "icon_2"
     });
     d.push({
@@ -41,7 +41,7 @@ function bookCase() {
             refreshPage(false);
             return 'hiker://empty';
         }, cfgfile, Juconfig),
-        img: "https://hikerfans.com/tubiao/messy/85.svg",
+        img: "http://123.56.105.145/tubiao/messy/85.svg",
         col_type: "icon_2"
     });
     for (let i = 0; i < 8; i++) {
